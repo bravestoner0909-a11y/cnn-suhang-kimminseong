b1b4d03c18d574c2bae87772eda0b6be05d6dda8
bravestoner0909-a11y <bravestoner0909@gmail.com>
2026-06-06 14:15:32 +0900

    config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Supabase 설정 파일
 // Vercel에 올리기 전에 아래 3개 값을 채우면, 선생님은 링크만 열어도 바로 기본 데이터가 연결됩니다.
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://zqdpfboagniulpqvukni.supabase.co/rest",
+  SUPABASE_URL: "https://zqdpfboagniulpqvukni.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpxZHBmYm9hZ25pdWxwcXZ1a25pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2ODI4NzAsImV4cCI6MjA5NjI1ODg3MH0.tk7HLbBg8vEIOV3v3i7-jD0aYXicHjCyy50XJedpFhU",
   BUCKET: "cnn-images"
 };
